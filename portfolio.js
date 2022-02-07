184c334610c3c6926013f3313308d2df30d555b9
Jack <58924024+jklein4800@users.noreply.github.com>
2022-02-07 02:04:13 -0500

    testing
    First upload, code and images aren't minified.

---
+++ portfolio.js
@@ -0,0 +1,18 @@
+const h1 = document.querySelector('h1');
+const li = document.querySelectorAll('li');
+
+function fadeIn () {
+  h1.style.opacity = '1';
+  li[0].style.opacity = '1';
+  li[1].style.opacity = '1';
+  li[2].style.opacity = '1';
+  li[3].style.opacity = '1';
+  li[4].style.opacity = '1';
+}
+
+
+window.onload = () => {
+  window.setTimeout(fadeIn(), 500);
+}
+
+
