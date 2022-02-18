303cb0cbcbfd944a7109dbc5f046420319a582b8
Jack <58924024+jklein4800@users.noreply.github.com>
2022-02-18 11:58:29 -0500

    mobile layout changed
    updated mobile layout to be more user friendly

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,19 @@
 const h1 = document.querySelector('h1');
 const li = document.querySelectorAll('li');
 
+
+
 function fadeIn () {
   h1.style.opacity = '1';
   li[0].style.opacity = '1';
   li[1].style.opacity = '1';
   li[2].style.opacity = '1';
-  li[3].style.opacity = '1';
-  li[4].style.opacity = '1';
+
+
+
+
+
+
 }
 
 
